951450dcf47f0c6a17e9ccbb92317b296c946c9f
andypotato <8491849+andypotato@users.noreply.github.com>
2020-12-31 16:23:23 +0800

    Remove unused vars

--- worker.js
+++ worker.js
@@ -4,11 +4,9 @@ const driver = require('./build/Release/js-inv-mpu.node');
 driver.setSampleFreq(workerData.sample);
 driver.setDlpf(workerData.filter);
 driver.startMpu();
-driver.readMpu((accel, gyro, comp, rot, t) => parentPort.postMessage
+driver.readMpu((accel, gyro, rot) => parentPort.postMessage
 ({
     accel: { x: accel[2], y: accel[1], z: accel[0] },
     gyro: { x: gyro[2], y: gyro[1], z: gyro[0] },
-    comp: { x: comp[2], y: comp[1], z: comp[0] },
-    rot: { y: rot[0], p: rot[1], r: rot[2] },
-    t: t
+    rot: { y: rot[0], p: rot[1], r: rot[2] }
 }));
